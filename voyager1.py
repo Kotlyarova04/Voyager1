0272e658adbdc152c480c8b129b541b3ddf8d57d
Kotlyarova04 <p.kotlyarova@g.nsu.ru>
2023-03-13 09:29:58 +0700

    project

--- voyager1.py
+++ voyager1.py
@@ -1,8 +1,8 @@
 import datetime
 import ru_local as ru
-spd = 38241
-dstnc = 16637000000
-radio_wave = 299792458
+SPD = 38241
+DSTNC = 16637000000
+RADIO_WAVE = 299792458
 print(ru.DATE)
 year = int(input())
 month = int(input())
@@ -10,10 +10,10 @@ day = int(input())
 date1 = datetime.date(2009, 9, 25)
 date2 = datetime.date(year, month, day)
 delta = date2 - date1
-distanceday = spd * 24
+distanceday = SPD * 24
 distanceperiod = distanceday * delta.days
-distancetosun = dstnc - distanceperiod
-delay = distancetosun / (radio_wave * 3600 / 1000 / 1.609)
+distancetosun = DSTNC - distanceperiod
+delay = distancetosun / (RADIO_WAVE * 3600 / 1000 / 1.609)
 print(delta.days, ru.DAYS)
 print(distanceday, ru.PER_DAY)
 print(distanceperiod, ru.PERIOD, delta.days, ru.DAYS)
