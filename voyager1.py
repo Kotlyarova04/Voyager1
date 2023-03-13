1793ed0c2bbf421974367485f7f89ab41730a768
Kotlyarova04 <p.kotlyarova@g.nsu.ru>
2023-03-13 17:22:52 +0700

    project

--- voyager1.py
+++ voyager1.py
@@ -4,19 +4,24 @@ import ru_local as ru
 SPD = 38241
 DSTNC = 16637000000
 RADIO_WAVE = 299792458
+
 print(ru.DATE)
-year = int(input())
-month = int(input())
-day = int(input())
+
+# Number of days that have passed.
+delta = int(input())
 date1 = datetime.date(2009, 9, 25)
-date2 = datetime.date(year, month, day)
-delta = date2 - date1
+# Date in n number of days.
+date2 = datetime.timedelta(days=delta)
+
+# Distance per day.
 distanceday = SPD * 24
-distanceperiod = distanceday * delta.days
-distancetosun = DSTNC + distanceperiod
-delay = distancetosun / (RADIO_WAVE * 3600 / 1000 / 1.609)
-print(delta.days, ru.DAYS)
+# Distance per period.
+distanceperiod = distanceday * date2.days
+distancesun = DSTNC + distanceperiod
+# Wireless delay in hours.
+delay = distancesun / (RADIO_WAVE * 3600 / 1000 / 1.609)
+
 print(distanceday, ru.PER_DAY)
-print(distanceperiod, ru.PERIOD, delta.days, ru.DAYS)
-print(distancetosun, ru.MILE, round(distancetosun * 1.609, 2), ru.KM, round(distancetosun * 1.609 / 149597870.7, 2), ru.AU)
+print(distanceperiod, ru.PERIOD, date2.days, ru.DAYS)
+print(distancesun, ru.MILE, round(distancesun * 1.609, 2), ru.KM, round(distancesun * 1.609 / 149597870.7, 2), ru.AU)
 print(ru.DELAY_HOURS, round(delay, 2))
